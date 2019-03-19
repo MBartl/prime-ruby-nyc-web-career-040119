4643d8512d2b73b1827225ee07136548e6c1aa18
Matthew Bartl <mattbartl@gmail.com>
2019-03-19 20:33:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,18 @@
 def prime?(number)
   if number < 2
     return false
+    
   elsif number == 2
     return true
+    
   else
     check = (number/2).floor
     while check > 1 do
       if number % check == 0
-      return false
+        return false
+      end
+      check -= 1
     end
-    check -= 1
+    return true
   end
-  return true
-end
 end

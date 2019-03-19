c73d85f7a7587536532945f276a4c34659a0457d
Matthew Bartl <mattbartl@gmail.com>
2019-03-19 19:55:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(number)
-  if number < 0
+  if number < 2
     return false
   end
-  if number > 1 && number < 9
+  if number == 2
+    return true
+  end
+  if number > 2 && number < 9
     if number % 2 == 0
       return false
     else
